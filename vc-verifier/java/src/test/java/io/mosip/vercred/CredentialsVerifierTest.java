86e1956ff26e59545156a7da6fc17743e371f778
Alka Prasad <prasadalka1998@gmail.com>
2024-08-08 12:45:44 +0530

    [INJIMOB-1679]: use junit5 and assert except messages in test
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- vc-verifier/java/src/test/java/io/mosip/vercred/CredentialsVerifierTest.java
+++ vc-verifier/java/src/test/java/io/mosip/vercred/CredentialsVerifierTest.java
@@ -1,7 +1,7 @@
 package io.mosip.vercred;
 
 import io.mosip.vercred.exception.ProofDocumentNotFoundException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -21,12 +21,15 @@ public class CredentialsVerifierTest {
         assertTrue(data);
     }
 
-    @Test(expected = ProofDocumentNotFoundException.class)
-    public void throwProofDocumentNotFoundExceptionIfVerifiableCredentialIsPaasedWithProofObject() throws IOException {
+    @Test
+    public void throwProofDocumentNotFoundExceptionIfVerifiableCredentialIsPassedWithProofObject() throws IOException {
         File file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "VerifiableCredentialWithoutProof.json");
         String credentialWithoutProof = new String(Files.readAllBytes(file.toPath()));
         CredentialsVerifier cv = new CredentialsVerifier();
-        cv.verifyCredentials(credentialWithoutProof);
+        Throwable exception = assertThrows(ProofDocumentNotFoundException.class, () -> {
+            cv.verifyCredentials(credentialWithoutProof);
+        });
+        assertEquals("Proof document is not available in the received credentials", exception.getMessage());
     }
 
     @Test
