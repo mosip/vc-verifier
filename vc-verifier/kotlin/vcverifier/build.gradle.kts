0b65332954d3d99e4f93bccd684bda45a3171823
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-09-19 10:33:03 +0530

    [INJIMOB-1709] - downgrade java to 17
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- vc-verifier/kotlin/vcverifier/build.gradle.kts
+++ vc-verifier/kotlin/vcverifier/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "io.mosip.vccred.vcverifier"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 23
@@ -20,11 +20,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     packaging {
         resources {
