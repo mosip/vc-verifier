a0d3b86125323da2910d35070e81aca49dd573c3
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-09-23 12:37:25 +0530

    Injimob 1709 kotlin vc verifier (#43)
    * [INJIMOB-1709] - move java codebase to kotlin
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1709] - update compilesdk to 34
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1709] - latest changes for error handling
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1709] - downgrade java to 17
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1709]: refactor specific code to integrate the library with react native project
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-1709] - refactor
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    Co-authored-by: Alka Prasad <prasadalka1998@gmail.com>

--- vc-verifier/kotlin/vcverifier/build.gradle.kts
+++ vc-verifier/kotlin/vcverifier/build.gradle.kts
@@ -20,11 +20,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     packaging {
         resources {
@@ -35,7 +35,11 @@ android {
 }
 
 dependencies {
-    implementation(libs.ldSignaturesJava)
+    implementation(libs.ldSignaturesJava) {
+        exclude(group = "com.apicatalog", module = "titanium-json-ld")
+    }
+    implementation (libs.titaniumJsonLd)
+    implementation(libs.okHttp)
     implementation(libs.bouncyCastle)
     implementation(libs.jsonldCommonJava)
     implementation(libs.nimbusJoseJwt)
