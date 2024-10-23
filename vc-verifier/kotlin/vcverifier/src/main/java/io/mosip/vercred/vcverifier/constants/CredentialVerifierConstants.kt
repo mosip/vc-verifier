499e6f06e49a1f32bd07944487feba4163f1250d
Alka Prasad <prasadalka1998@gmail.com>
2024-10-23 08:57:02 +0530

    [INJIMOB-2202]: add support for verification of sunbird VC
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/constants/CredentialVerifierConstants.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/constants/CredentialVerifierConstants.kt
@@ -4,6 +4,8 @@ package io.mosip.vercred.vcverifier.constants
 object CredentialVerifierConstants {
 
     const val PUBLIC_KEY_PEM = "publicKeyPem"
+    const val PUBLIC_KEY_MULTIBASE = "publicKeyMultibase"
+    const val VERIFICATION_METHOD = "verificationMethod"
 
     const val PSS_PARAM_SHA_256 = "SHA-256"
     const val PSS_PARAM_MGF1 = "MGF1"
@@ -19,6 +21,12 @@ object CredentialVerifierConstants {
     const val JWS_RS256_SIGN_ALGO_CONST = "RS256"
     const val JWS_EDDSA_SIGN_ALGO_CONST = "EdDSA"
 
+    const val RSA_SIGNATURE = "RsaSignature2018"
+    const val ED25519_SIGNATURE_2018 = "Ed25519Signature2018"
+    const val ED25519_SIGNATURE_2020 = "Ed25519Signature2020"
+
     const val EXCEPTION_DURING_VERIFICATION = "Exception during Verification: "
     const val VERIFICATION_FAILED = "Verification Failed"
+
+    const val DER_PUBLIC_KEY_PREFIX = "302a300506032b6570032100"
 }
