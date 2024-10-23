c7b569f7b51ebe01636d2b29a05eb8de18bf1bc5
BalachandarG <balachandar.g@thoughtworks.com>
2024-10-23 12:44:52 +0530

    [INJIMOB-1325]: Updating the method names and deprecating the exisitng method.
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- vc-verifier/js/src/CredentialsVerifier.js
+++ vc-verifier/js/src/CredentialsVerifier.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import {validate} from "./validator/LdpValidator.js";
 import {Errors} from "./constant/ValidatorConstants.js";
 import {isNotNullOrEmpty} from "./validator/Utils.js";
+import {preProcessVerifiableCredential} from "./utils/CredentialUtils.js";
 
 
 const getPublicKeyFromVerificationMethod = async (url) => {
@@ -41,38 +42,66 @@ const verifyCredentialSignature = (jwsHeaderAlgoName, publicKey, actualData, sig
     else if (jwsHeaderAlgoName === Constants.JWS_RS256_SIGN_ALGO_CONST) {
         isVerified = crypto.verify('RSA-SHA256', actualData, publicKey, signature);
     }
+    else {
+        throw new Error('Verification algorithm not supported');
+    }
     return isVerified;
 }
 
+/**
+ * @deprecated This method has been deprecated because it is not extensible for future use cases of supporting different VC format's verification
+ * Please use verify(credentials, format) instead, which is designed for supporting different VC formats.
+ * This method only supports LDP VC format
+ */
 export const verifyCredentials = async (credential) => {
     try {
-        const validationErrorMessage = validate(credential)
+        preProcessVerifiableCredential(credential);
+        return await verifySignature(credential)
+    } catch (error) {
+        throw error;
+    }
+}
 
-        if(isNotNullOrEmpty(validationErrorMessage) && (validationErrorMessage !== Errors.ERROR_VC_EXPIRED)){
-            verificationFailure(validationErrorMessage)
+/**
+ * Validates the provided credentials against the specified format and Verifies the Signature.
+ *
+ * @param {Object} credential - The credentials to verify, containing necessary fields.
+ * @param {string} credentialFormat - The expected format for the credentials (e.g., 'ldp').
+ * @returns {Promise<Object>} The result of the verification, including a status and any relevant messages.
+ */
+export const verify = async (credential, credentialFormat) => {
+    try {
+        const validationMessage = validate(credential)
+
+        if(isNotNullOrEmpty(validationMessage) && (validationMessage !== Errors.ERROR_VC_EXPIRED)){
+            verificationFailure(validationMessage)
         }
-        const { signature, header } = jws.decode(credential.proof.jws);
-        const decodedSignature = Buffer.from(signature, 'base64');
-        const publicKeyObject = await getPublicKeyFromVerificationMethod(credential.proof.verificationMethod);
-        const canonicalisedCredential = await URDNA2015Canonicalizer(credential);
-        const inputData = getJwsSigningInput(header, canonicalisedCredential);
-        const verificationResult = verifyCredentialSignature(header.alg, publicKeyObject, inputData, decodedSignature);
+        const verificationResult = await verifySignature(credential)
         if(!verificationResult) {
             return verificationFailure(`${Errors.SIGNATURE_VERIFICATION_FAILED}`)
         }
-        return verificationSuccess(validationErrorMessage)
+        return verificationSuccess(validationMessage)
 
     } catch (error) {
         return verificationFailure(`${Errors.EXCEPTION_DURING_VERIFICATION}${error.message}`)
     }
 }
 
+const verifySignature = async (credential) => {
+    const { signature, header } = jws.decode(credential.proof.jws);
+    const decodedSignature = Buffer.from(signature, 'base64');
+    const publicKeyObject = await getPublicKeyFromVerificationMethod(credential.proof.verificationMethod);
+    const canonicalisedCredential = await URDNA2015Canonicalizer(credential);
+    const inputData = getJwsSigningInput(header, canonicalisedCredential);
+    return verifyCredentialSignature(header.alg, publicKeyObject, inputData, decodedSignature);
+}
+
 const verificationSuccess = (message) => ({
     verificationStatus: true,
-    verificationErrorMessage: message ? message: ""
+    verificationMessage: message ? message: ""
 });
 
-const verificationFailure = (error) => ({
+const verificationFailure = (errorMessage) => ({
     verificationStatus: false,
-    verificationErrorMessage: error
+    verificationMessage: errorMessage
 });
