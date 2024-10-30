30f1e8e6bd65a9f5a868f87544093b8eb5b84176
BalachandarG <balachandar.g@thoughtworks.com>
2024-10-30 16:21:07 +0530

    [INJIMOB-2242]: Adding Static method in CredentialFormat to get the enum based on the value
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/constants/CredentialFormat.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/constants/CredentialFormat.kt
@@ -2,5 +2,11 @@ package io.mosip.vercred.vcverifier.constants
 
 enum class CredentialFormat(val value: String) {
     LDP_VC("ldp_vc"),
-    MSO_MDOC("mso_mdoc"),
+    MSO_MDOC("mso_mdoc");
+
+    companion object {
+        fun fromValue(value: String): CredentialFormat? {
+            return values().find { it.value == value }
+        }
+    }
 }
