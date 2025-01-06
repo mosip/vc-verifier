9984247691479af80eb14dbbb544269e3f020a63
Alka Prasad <Alka1703@users.noreply.github.com>
2025-01-06 15:01:26 +0530

    [INJIMOB-2580]- Refactor the library  (#61)
    * [INJIMOB-2580]: refactor LDP verifier to take any combination of public key and assertion method
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: remove timeouts from the tests
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: refactor tests to remove duplicate method calls with same parameters in same test
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: remove timeouts from the tests
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: refactor public key fetching implementation
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: refactor VC fetching from file in test
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: add timeouts in the verification test
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: add VC files for test
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580: add comments for proofValue verification
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-2580]: rename folder structure for VCs in resources
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    ---------
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/verifier/LdpVerifier.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/verifier/LdpVerifier.kt
@@ -1,46 +1,25 @@
 package io.mosip.vercred.vcverifier.credentialverifier.verifier
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ObjectNode
 import com.nimbusds.jose.JWSObject
 import foundation.identity.jsonld.ConfigurableDocumentLoader
 import foundation.identity.jsonld.JsonLDObject
 import info.weboftrust.ldsignatures.LdProof
 import info.weboftrust.ldsignatures.canonicalizer.URDNA2015Canonicalizer
 import info.weboftrust.ldsignatures.util.JWSUtil
-import io.ipfs.multibase.Base58
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.DER_PUBLIC_KEY_PREFIX
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.ED25519_ALGORITHM
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.ED25519_SIGNATURE_2018
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.ED25519_SIGNATURE_2020
+import io.ipfs.multibase.Multibase
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_EDDSA_SIGN_ALGO_CONST
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_PS256_SIGN_ALGO_CONST
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_RS256_SIGN_ALGO_CONST
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.PUBLIC_KEY_MULTIBASE
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.RSA_ALGORITHM
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.RSA_SIGNATURE
-import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.VERIFICATION_METHOD
 import io.mosip.vercred.vcverifier.exception.PublicKeyNotFoundException
 import io.mosip.vercred.vcverifier.exception.SignatureVerificationException
 import io.mosip.vercred.vcverifier.exception.UnknownException
+import io.mosip.vercred.vcverifier.publicKey.PublicKeyGetterFactory
 import io.mosip.vercred.vcverifier.signature.SignatureVerifier
 import io.mosip.vercred.vcverifier.signature.impl.ED25519SignatureVerifierImpl
 import io.mosip.vercred.vcverifier.signature.impl.PS256SignatureVerifierImpl
 import io.mosip.vercred.vcverifier.signature.impl.RS256SignatureVerifierImpl
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.bouncycastle.util.encoders.Hex
-import org.bouncycastle.util.io.pem.PemReader
-import java.io.StringReader
-import java.net.URI
-import java.security.KeyFactory
-import java.security.KeyManagementException
-import java.security.PublicKey
 import java.security.Security
-import java.security.cert.CertificateException
-import java.security.spec.X509EncodedKeySpec
 import java.util.logging.Logger
 
 
@@ -49,17 +28,12 @@ class LdpVerifier {
     private val logger = Logger.getLogger(LdpVerifier::class.java.name)
 
     private var provider: BouncyCastleProvider = BouncyCastleProvider()
+
     private val SIGNATURE_VERIFIER: Map<String, SignatureVerifier> = mapOf(
         JWS_PS256_SIGN_ALGO_CONST to PS256SignatureVerifierImpl(),
         JWS_RS256_SIGN_ALGO_CONST to RS256SignatureVerifierImpl(),
         JWS_EDDSA_SIGN_ALGO_CONST to ED25519SignatureVerifierImpl()
     )
-    private val PUBLIC_KEY_ALGORITHM: Map<String, String> = mapOf(
-        RSA_SIGNATURE to RSA_ALGORITHM,
-        ED25519_SIGNATURE_2018 to ED25519_ALGORITHM,
-        ED25519_SIGNATURE_2020 to ED25519_ALGORITHM
-    )
-    private val RESOLVER_API = "https://resolver.identity.foundation/1.0/identifiers/"
 
     init {
         Security.addProvider(provider);
@@ -74,28 +48,30 @@ class LdpVerifier {
 
         return try {
             val ldProof: LdProof = LdProof.getFromJsonLDObject(vcJsonLdObject)
-            val canonicalizer = URDNA2015Canonicalizer()
-            val canonicalHashBytes: ByteArray = canonicalizer.canonicalize(ldProof, vcJsonLdObject)
 
-            if(!ldProof.jws.isNullOrEmpty()) {
+            val canonicalizer = URDNA2015Canonicalizer()
+            val canonicalHashBytes = canonicalizer.canonicalize(ldProof, vcJsonLdObject)
+
+            val verificationMethod = ldProof.verificationMethod
+            val publicKeyObj = PublicKeyGetterFactory().get(verificationMethod)
+
+            if (!ldProof.jws.isNullOrEmpty()) {
                 val signJWS: String = ldProof.jws
-                val jwsObject: JWSObject = JWSObject.parse(signJWS)
-                val signature: ByteArray = jwsObject.signature.decode()
-                val actualData: ByteArray = JWSUtil.getJwsSigningInput(jwsObject.header, canonicalHashBytes)
-                val publicKeyObj = getPublicKeyFromHttpVerificationMethod(ldProof.verificationMethod, ldProof.type)
-                val signatureVerifier: SignatureVerifier = SIGNATURE_VERIFIER[jwsObject.header.algorithm.name]!!
-                return signatureVerifier.verify(publicKeyObj!!, actualData, signature, provider)
+                val jwsObject = JWSObject.parse(signJWS)
+                val signature = jwsObject.signature.decode()
+                val actualData = JWSUtil.getJwsSigningInput(jwsObject.header, canonicalHashBytes)
+                val signatureVerifier = SIGNATURE_VERIFIER[jwsObject.header.algorithm.name]!!
+                return signatureVerifier.verify(publicKeyObj, actualData, signature, provider)
             }
 
-            else if(!ldProof.proofValue.isNullOrEmpty()){
-                val proofValue: String = ldProof.proofValue
-                val signature = Base58.decode(proofValue.substring(1))
-                val publicKeyObj = getPublicKeyFromDidVerificationMethod(ldProof.verificationMethod, ldProof.type)
-                val signatureVerifier: SignatureVerifier = ED25519SignatureVerifierImpl()
-                return signatureVerifier.verify(publicKeyObj!!, canonicalHashBytes, signature, provider)
+            //Currently we are getting proofValue only in ED25519Signature2020 sunbird VC
+            else if (!ldProof.proofValue.isNullOrEmpty()) {
+                val proofValue = ldProof.proofValue
+                val signature = Multibase.decode(proofValue)
+                val signatureVerifier = ED25519SignatureVerifierImpl()
+                return signatureVerifier.verify(publicKeyObj, canonicalHashBytes, signature, provider)
             }
             false
-
         } catch (e: Exception) {
             when (e) {
                 is PublicKeyNotFoundException,
@@ -106,79 +82,6 @@ class LdpVerifier {
             }
         }
     }
-
-    @Throws(CertificateException::class, KeyManagementException::class)
-    private fun getPublicKeyFromHttpVerificationMethod(verificationMethod: URI, signatureSuite: String ): PublicKey? {
-        return try {
-            val okHttpClient = OkHttpClient.Builder().build().newBuilder().build()
-            val request = Request.Builder()
-                .url(verificationMethod.toURL())
-                .get()
-                .build()
-
-            val response = okHttpClient.newCall(request).execute()
-            response.body?.let { responseBody ->
-                val objectMapper = ObjectMapper()
-                val jsonNode = objectMapper.readTree(responseBody.string())
-                if (jsonNode.isObject) {
-                    val responseObjectNode = jsonNode as ObjectNode
-                    val publicKeyPem =
-                        responseObjectNode[CredentialVerifierConstants.PUBLIC_KEY_PEM].asText()
-                    val strReader = StringReader(publicKeyPem)
-                    val pemReader = PemReader(strReader)
-                    val pemObject = pemReader.readPemObject()
-                    val pubKeyBytes = pemObject.content
-                    val pubKeySpec = X509EncodedKeySpec(pubKeyBytes)
-                    val keyFactory = KeyFactory.getInstance(PUBLIC_KEY_ALGORITHM[signatureSuite], provider)
-                    keyFactory.generatePublic(pubKeySpec)
-                } else
-                    throw PublicKeyNotFoundException("Public key object is null")
-            }
-        } catch (e: Exception) {
-            logger.severe("Error Generating public key object$e")
-            throw PublicKeyNotFoundException("Public key object is null")
-        }
-    }
-
-    private fun getPublicKeyFromDidVerificationMethod(verificationMethod: URI, signatureSuite: String): PublicKey? {
-        val resolverUrl = "$RESOLVER_API$verificationMethod"
-        try {
-            val request = Request.Builder()
-                .url(URI(resolverUrl).toURL())
-                .get()
-                .build()
-            val response = OkHttpClient.Builder().build().newCall(request).execute()
-            response.body?.use { responseBody ->
-                val jsonNode = ObjectMapper().readTree(responseBody.string())
-                if (jsonNode.isObject) {
-                    val publicKeyMultibase = getPublicKeyMultiBase(jsonNode as ObjectNode)
-                    val rawPublicKeyWithHeader = Base58.decode(publicKeyMultibase.substring(1))
-                    if (isEd25519PublicKey(rawPublicKeyWithHeader)) {
-                        val rawPublicKey = rawPublicKeyWithHeader.copyOfRange(2, rawPublicKeyWithHeader.size)
-                        val publicKey = Hex.decode(DER_PUBLIC_KEY_PREFIX) + rawPublicKey
-                        val pubKeySpec = X509EncodedKeySpec(publicKey)
-                        val keyFactory = KeyFactory.getInstance(PUBLIC_KEY_ALGORITHM[signatureSuite], provider)
-                        return keyFactory.generatePublic(pubKeySpec)
-                    }
-                }
-            }
-            throw PublicKeyNotFoundException("Public key object is null")
-        } catch (e: Exception) {
-            logger.severe("Error generating public key object $e")
-            throw PublicKeyNotFoundException("Public key object is null")
-        }
-    }
-
-    private fun getPublicKeyMultiBase(responseObjectNode: ObjectNode): String =
-        responseObjectNode.get("didDocument")
-            .get(VERIFICATION_METHOD)[0].get(PUBLIC_KEY_MULTIBASE).asText()
-
-    //Ref: https://w3c.github.io/vc-di-eddsa/#multikey
-    private fun isEd25519PublicKey(rawPublicKeyWithHeader: ByteArray) =
-        rawPublicKeyWithHeader.size > 2 &&
-                rawPublicKeyWithHeader[0] == 0xed.toByte() &&
-                rawPublicKeyWithHeader[1] == 0x01.toByte()
-
 
     private fun getConfigurableDocumentLoader(): ConfigurableDocumentLoader {
         val confDocumentLoader = ConfigurableDocumentLoader()
---
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/publicKey/PublicKeyGetterFactory.kt
@@ -0,0 +1,19 @@
+package io.mosip.vercred.vcverifier.publicKey
+
+import io.mosip.vercred.vcverifier.exception.PublicKeyTypeNotSupportedException
+import io.mosip.vercred.vcverifier.publicKey.impl.DidWebPublicKeyGetter
+import io.mosip.vercred.vcverifier.publicKey.impl.HttpsPublicKeyGetter
+import java.net.URI
+import java.security.PublicKey
+
+
+class PublicKeyGetterFactory {
+    fun get(verificationMethod: URI): PublicKey {
+        val verificationMethodStr = verificationMethod.toString()
+        return when {
+            verificationMethodStr.startsWith("did:web") -> DidWebPublicKeyGetter().get(verificationMethod)
+            verificationMethodStr.startsWith("http") -> HttpsPublicKeyGetter().get(verificationMethod)
+            else -> throw PublicKeyTypeNotSupportedException("Public Key type is not supported")
+        }
+    }
+}
