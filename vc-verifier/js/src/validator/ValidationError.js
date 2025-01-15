b188f88c3c9a96653c16a536b66d4c4ed066c7fd
BalachandarG <balachandar.g@thoughtworks.com>
2025-01-15 15:17:21 +0530

    [INJIMOB-2712]: Remove js package and vc-verifier tool
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- vc-verifier/js/src/validator/ValidationError.js
+++
@@ -1,7 +0,0 @@
-export class ValidationError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
