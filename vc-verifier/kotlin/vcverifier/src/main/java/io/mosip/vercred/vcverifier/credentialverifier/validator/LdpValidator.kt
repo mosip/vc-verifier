0ccfe2e82679121822b540dab4c0c04c9acbd176
BalachandarG <balachandar.g@thoughtworks.com>
2025-01-15 15:44:25 +0530

    [INJIMOB-2712]: Refactor expiry date condition
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/validator/LdpValidator.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/validator/LdpValidator.kt
@@ -70,18 +70,25 @@ class LdpValidator {
                 DATA_MODEL_1_1 -> {
                     validateV1SpecificFields(vcJsonObject)
                     validateCommonFields(vcJsonObject)
-                    val expirationMessage = if (vcJsonObject.has(EXPIRATION_DATE) && dateUtils.isVCExpired(vcJsonObject.optString(
-                            EXPIRATION_DATE))) ERROR_MESSAGE_VC_EXPIRED else ""
-                    val verificationStatusCode = if (vcJsonObject.has(EXPIRATION_DATE) && dateUtils.isVCExpired(vcJsonObject.optString(
-                            EXPIRATION_DATE))) ERROR_CODE_VC_EXPIRED else ""
+                    var expirationMessage = ""
+                    var verificationStatusCode = ""
+                    if (vcJsonObject.has(EXPIRATION_DATE) && dateUtils.isVCExpired(vcJsonObject.optString(
+                            EXPIRATION_DATE))) {
+                        expirationMessage = ERROR_MESSAGE_VC_EXPIRED
+                        verificationStatusCode = ERROR_CODE_VC_EXPIRED
+                    }
                     return ValidationStatus(expirationMessage, verificationStatusCode)
                 }
                 DATA_MODEL_2_0 -> {
                     validateV2SpecificFields(vcJsonObject)
                     validateCommonFields(vcJsonObject)
-                    val expirationMessage = if (vcJsonObject.has(VALID_UNTIL) && dateUtils.isVCExpired(vcJsonObject.optString(VALID_UNTIL))) ERROR_MESSAGE_VC_EXPIRED else ""
-                    val verificationStatusCode = if (vcJsonObject.has(VALID_UNTIL) && dateUtils.isVCExpired(vcJsonObject.optString(
-                            VALID_UNTIL))) ERROR_CODE_VC_EXPIRED else ""
+                    var expirationMessage = ""
+                    var verificationStatusCode = ""
+                    if (vcJsonObject.has(VALID_UNTIL) && dateUtils.isVCExpired(vcJsonObject.optString(
+                            VALID_UNTIL))) {
+                        expirationMessage = ERROR_MESSAGE_VC_EXPIRED
+                        verificationStatusCode = ERROR_CODE_VC_EXPIRED
+                    }
                     return ValidationStatus(expirationMessage, verificationStatusCode)
                 }
                 else -> {
