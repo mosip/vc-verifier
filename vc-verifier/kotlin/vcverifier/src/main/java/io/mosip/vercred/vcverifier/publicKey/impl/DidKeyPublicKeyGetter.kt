e850f9cc4618ae95f6d33e4a139cb01262a5b14f
Sreenadh S <32409698+sree96@users.noreply.github.com>
2025-06-11 12:16:02 +0530

    [INJIVER-991] - refactor to avoid unwanted JWK parsing to get the key
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/publicKey/impl/DidKeyPublicKeyGetter.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/publicKey/impl/DidKeyPublicKeyGetter.kt
@@ -1,11 +1,8 @@
-import com.nimbusds.jose.jwk.JWK
 import io.ipfs.multibase.Multibase
 import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.JWS_EDDSA_SIGN_ALGO_CONST
 import io.mosip.vercred.vcverifier.exception.SignatureNotSupportedException
 import io.mosip.vercred.vcverifier.exception.UnknownException
 import io.mosip.vercred.vcverifier.publicKey.PublicKeyGetter
-import io.mosip.vercred.vcverifier.utils.Base64Decoder
-import io.mosip.vercred.vcverifier.utils.Base64Encoder
 import org.bouncycastle.asn1.edec.EdECObjectIdentifiers
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
@@ -16,13 +13,10 @@ import java.security.PublicKey
 import java.security.spec.InvalidKeySpecException
 import java.security.spec.X509EncodedKeySpec
 import java.util.Arrays
-import java.util.Base64
 
 
 class DidKeyPublicKeyGetter : PublicKeyGetter {
     private var provider: BouncyCastleProvider = BouncyCastleProvider()
-    private var b64Decoder: Base64Decoder = Base64Decoder()
-    private var b64Encoder: Base64Encoder = Base64Encoder()
     override fun get(verificationMethod: URI): PublicKey {
 
         val decodedKey =
@@ -38,14 +32,7 @@ class DidKeyPublicKeyGetter : PublicKeyGetter {
         if ((decodedKey[0] == 0xed.toByte() && decodedKey[1] == 0x01.toByte()) && decodedKey.size == 34) {
             try {
 
-                val edKey: JWK = JWK.parse(
-                    mapOf(
-                        "kty" to "OKP",
-                        "crv" to "Ed25519",
-                        "x" to b64Encoder.encodeToBase64UrlFormatEncoded(Arrays.copyOfRange(decodedKey, 2, 34))
-                    )
-                )
-                val publicKeyBytes = b64Decoder.decodeFromBase64UrlFormatEncoded(edKey.toOctetKeyPair().x.toString())
+                val publicKeyBytes = Arrays.copyOfRange(decodedKey, 2, 34)
                 val algorithmIdentifier = AlgorithmIdentifier(EdECObjectIdentifiers.id_Ed25519)
                 val subjectPublicKeyInfo = SubjectPublicKeyInfo(algorithmIdentifier, publicKeyBytes)
                 val encodedKey = subjectPublicKeyInfo.encoded
