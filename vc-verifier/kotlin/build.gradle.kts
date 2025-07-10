a067f5f86758bf008e00e905318092a44debdb82
Prafulrakhade <prafulrakhade02@gmail.com>
2025-07-11 00:57:21 +0530

    [MOSIP-41674] central migration changes
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- vc-verifier/kotlin/build.gradle.kts
+++ vc-verifier/kotlin/build.gradle.kts
@@ -4,16 +4,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.dokka ) apply false
-    `maven-publish`
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-}
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(System.getenv("OSSRH_USER"))
-            password.set(System.getenv("OSSRH_SECRET"))
-        }
-    }
+    `maven-publish` apply false
 }
