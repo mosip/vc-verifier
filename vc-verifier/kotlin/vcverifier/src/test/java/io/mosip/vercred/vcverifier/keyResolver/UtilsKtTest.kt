eec19eacd576fc519f7980c884909217cf03df8e
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-08-19 16:49:47 +0530

    [INJIMOB-3408] refactor: remove redundant classes added due to rebase
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- vc-verifier/kotlin/vcverifier/src/test/java/io/mosip/vercred/vcverifier/keyResolver/UtilsKtTest.kt
+++ vc-verifier/kotlin/vcverifier/src/test/java/io/mosip/vercred/vcverifier/keyResolver/UtilsKtTest.kt
@@ -1,12 +1,46 @@
 package io.mosip.vercred.vcverifier.keyResolver
 
+import io.mosip.vercred.vcverifier.constants.CredentialVerifierConstants.ES256K_KEY_TYPE_2019
 import io.mosip.vercred.vcverifier.exception.PublicKeyResolutionFailedException
 import io.mosip.vercred.vcverifier.testHelpers.assertPublicKey
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.security.PublicKey
+import java.security.interfaces.ECPublicKey
 
 class UtilsKtTest {
+    @Test
+    fun `test EC secp256k1 public key extraction`() {
+        val jwkString = """
+    {
+        "kty": "EC",
+        "crv": "secp256k1",
+        "x": "STRMr8BN3ToqGYWQExEm5-mjyiSqq9iGs600-4UMiZY",
+        "y": "wMC2jyYYA1UPz5TjeHRkSAZV6y6_C5oyCPsudWtFQPM"
+    }
+    """.trimIndent()
+        val publicKey: PublicKey = getPublicKeyFromJWK(jwkString, ES256K_KEY_TYPE_2019)
+
+        assertNotNull(publicKey)
+        assertTrue(publicKey is ECPublicKey)
+        assertEquals("EC", publicKey.algorithm)
+    }
+
+    @Test
+    fun `should correctly generate PublicKey from valid compressed secp256k1 hex`() {
+        // Valid compressed secp256k1 public key (33 bytes)
+        val compressedHexKey = "034ee0f670fc96bb75e8b89c068a1665007a41c98513d6a911b6137e2d16f1d300"
+
+        val publicKey = getPublicKeyFromHex(compressedHexKey, ES256K_KEY_TYPE_2019)
+
+        assertNotNull(publicKey, "Public key should not be null")
+        assertTrue(publicKey is ECPublicKey, "Returned key should be an instance of ECPublicKey")
+        assertEquals("EC", publicKey.algorithm)
+    }
+
     @Test
     fun `should return public key for Ed public key in JWK format`() {
         val edPublicKeyJwk = mapOf(
