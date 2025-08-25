e520f67a87777ec71470fe1cd9d491c2e1bcc8a1
swatigoel <meet2swati@gmail.com>
2025-08-25 13:22:11 +0530

    [INJIMOB-3509] - Remove revocation feature support for ldp_vc
    Signed-off-by: swatigoel <meet2swati@gmail.com>

--- vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/types/LdpVerifiableCredential.kt
+++ vc-verifier/kotlin/vcverifier/src/main/java/io/mosip/vercred/vcverifier/credentialverifier/types/LdpVerifiableCredential.kt
@@ -1,7 +1,6 @@
 package io.mosip.vercred.vcverifier.credentialverifier.types
 
 import io.mosip.vercred.vcverifier.credentialverifier.VerifiableCredential
-import io.mosip.vercred.vcverifier.credentialverifier.revocation.LdpRevokeChecker
 import io.mosip.vercred.vcverifier.credentialverifier.validator.LdpValidator
 import io.mosip.vercred.vcverifier.credentialverifier.verifier.LdpVerifier
 import io.mosip.vercred.vcverifier.data.ValidationStatus
@@ -14,10 +13,4 @@ class LdpVerifiableCredential : VerifiableCredential {
     override fun verify(credential: String): Boolean {
         return LdpVerifier().verify(credential)
     }
-
-    override fun isRevoked(credential: String): Boolean {
-        return LdpRevokeChecker().isRevoked(credential)
-    }
-
-
 }
